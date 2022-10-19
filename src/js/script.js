73bdb90ab81fb8cc1fd1c1faef330f71d9f914d3
Konstantin Tenman <ktenman@gmail.com>
2022-10-19 16:34:02 +0300

    Show post title

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
-window.onload = function() {
-
+window.onload = function () {
 
 
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -7,8 +6,15 @@ window.onload = function() {
         .then(json => {
             console.log(json);
 
+            for (const jsonElement of json) {
+                let divElement = document.createElement("div");
+                let title = document.createElement("h3");
+                divElement.className = "post"
+                title.innerHTML = jsonElement.title
+                divElement.appendChild(title)
+                document.body.appendChild(divElement)
+            }
+
         })
-
-
 
 }
